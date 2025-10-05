43fee731d381e6b9c5acacf72d1ea11efe8e2564
Ksr123456789 <kuldeeprawat6277@gmail.com>
2025-10-05 11:38:29 +0530

    wrote register controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,74 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+export const registerUser = asyncHandler(async (req, res) => {
+  // get user details from frontend
+  //validation - not empty
+  //check if user already exists: username, email
+  //check for images, check for avator
+  //upload them to cloudinary, avator
+  //create user object - create entry in db
+  //remove password and refresh token field from response
+  //check for user creation
+  //return response
+
+  const { fullName, email, username, password } = req.body;
+  console.log("this is request.body : ", req.body);
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "all fields are required");
+  }
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "user with email or username already exists");
+  }
+  console.log("this is req.files : ", req.files);
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0];
+  }
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "user registered successfully"));
+});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`server is running at port`);
+      console.log(`server is running at port 8000`);
     });
   })
   .catch((err) => {
